b5de2dacf1fa4591180523c5afef315d9723cdfc
Ash M <43498948+ash-m-m@users.noreply.github.com>
2022-10-24 18:03:26 -0400

    Update brew-install.sh

--- brew-install.sh
+++ brew-install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/ash/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/ash/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew cask install iterm2
+brew install iterm2
 brew install gh
